b47ba296c270bbe06c053fc7874761d7f92d32a8
unknown <shahrom@DESKTOP-7ONCABA.nc2.mil>
2020-10-07 12:48:43 +0800

    Updates

---
+++ precache-manifest.d9cc26542dcff2d1b26b5ff533f8d0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306920dd10b1416f91464f1701ecff05",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4234df742e2461b69a03",
+    "url": "/weather-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a6bf7ccecbbff4619e0",
+    "url": "/weather-app/static/js/2.10d4690c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.10d4690c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4234df742e2461b69a03",
+    "url": "/weather-app/static/js/main.1fc38fb1.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  }
+]);
